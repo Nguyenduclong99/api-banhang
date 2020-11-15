ee2586c86a3fc0def0405959e036beeb0d17f2da
Nguyenduclong99 <55075056+Nguyenduclong99@users.noreply.github.com>
2020-11-16 01:11:34 +0700

    update

--- API/Controllers/ItemController.cs
+++ API/Controllers/ItemController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using BLL;
@@ -15,15 +16,55 @@ namespace API.Controllers
     public class ItemController : ControllerBase
     {
         private IItemBusiness _itemBusiness;
-        public ItemController(IItemBusiness itemBusiness)
+        private string _path;
+        public ItemController(IItemBusiness itemBusiness, IConfiguration configuration)
         {
             _itemBusiness = itemBusiness;
+            _path = configuration["AppSettings:PATH"];
+        }
+
+        public string SaveFileFromBase64String(string RelativePathFileName, string dataFromBase64String)
+        {
+            if (dataFromBase64String.Contains("base64,"))
+            {
+                dataFromBase64String = dataFromBase64String.Substring(dataFromBase64String.IndexOf("base64,", 0) + 7);
+            }
+            return WriteFileToAuthAccessFolder(RelativePathFileName, dataFromBase64String);
+        }
+        public string WriteFileToAuthAccessFolder(string RelativePathFileName, string base64StringData)
+        {
+            try
+            {
+                string result = "";
+                string serverRootPathFolder = _path;
+                string fullPathFile = $@"{serverRootPathFolder}\{RelativePathFileName}";
+                string fullPathFolder = System.IO.Path.GetDirectoryName(fullPathFile);
+                if (!Directory.Exists(fullPathFolder))
+                    Directory.CreateDirectory(fullPathFolder);
+                System.IO.File.WriteAllBytes(fullPathFile, Convert.FromBase64String(base64StringData));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         [Route("create-item")]
         [HttpPost]
         public ItemModel CreateItem([FromBody] ItemModel model)
         {
+
+            if (model.Image != null)
+            {
+                var arrData = model.Image.Split(';');
+                if (arrData.Length == 3)
+                {
+                    var savePath = $@"{arrData[0]}";
+                    model.Image = $"{savePath}";
+                    SaveFileFromBase64String(savePath, arrData[2]);
+                }
+            }
             _itemBusiness.Create(model);
             return model;
         }
@@ -31,6 +72,17 @@ namespace API.Controllers
         [HttpPost]
         public ItemModel Edit(int id, [FromBody] ItemModel model)
         {
+
+            if (model.Image != null)
+            {
+                var arrData = model.Image.Split(';');
+                if (arrData.Length == 3)
+                {
+                    var savePath = $@"{arrData[0]}";
+                    model.Image = $"{savePath}";
+                    SaveFileFromBase64String(savePath, arrData[2]);
+                }
+            }
             _itemBusiness.Edit(id, model);
             return model;
         }
